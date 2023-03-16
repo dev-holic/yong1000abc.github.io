d9260165d47ffbf6bc669f62b5f256d91d331ffe
yong <yong1000abc@gmail.com>
2023-03-16 20:26:19 +0900

    init prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import React from 'react'
+import React from 'react';
 
-function App() {
-    return (
-        <div>
-            App
-        </div>
-    )
+export function App() {
+  return <div>App</div>;
 }
-
-export default App

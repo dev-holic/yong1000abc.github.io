f09e1a726b550fa551906da553ab61a77f72b9e9
yong <yong1000abc@gmail.com>
2023-03-25 23:21:38 +0900

    set deploy config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
 export function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <div>
+        <a href="/first-githubpages-app">first-githubpages-app</a>
+      </div>
+      <div>
+        <a href="/second-githubpages-app">second-githubpages-app</a>
+      </div>
+    </div>
+  );
 }
